48ad921cd3a689c1e59e7c35c595b8dd7104cb64
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-04-07 08:58:04 +0200

    task 2 is done

--- src/2.ts
+++ src/2.ts
@@ -33,6 +33,10 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, salary: number){
+    super(name, 'Manager', salary);
+    this.salary += 10000
+  }
 }
 
 
