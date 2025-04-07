e384eefc01f74c67f4b385a6120079cc18a6c7b7
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-04-07 09:21:47 +0200

    task 4 is done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature
+    }
+}
+
+
+class Person {
+    constructor(private key: Key){
+        this.key = key
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+class House {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            console.log('Door opened');
+        } else {
+            console.log('Door not opened');
+        }
+    }
+}
+
+class MyHouse extends House {
+ 
+    comeIn(person: Person): void {
+        console.log(`${person} came in the house`);
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
